36ccb5f398c17f4fcaed56a2740e233d540b6790
unknown <thuyle8824@gmail.com>
2020-07-07 17:12:09 +0700

    add

--- src/FindMin.java
+++ src/FindMin.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class FindMin {
     public static int minValue(int[] array){
         int min=array[0];
@@ -7,18 +9,23 @@ public class FindMin {
         }
         return min;
     }
-    public static int indexMin(int[] array) {
-        int index=0;
-        for (int j=0;j<array.length;j++) {
-            if (minValue(array)==array[j])
-                index=j+1;
-        }
-        return index;
-    }
     public static void main(String[] args) {
-        int []arr = {4, 12, 7, 8, 1, 6, 9};
-        int min = minValue(arr);
-        int ind = indexMin(arr);
-        System.out.println("The smallest element in the array is: " +min+" at position "+ ind);
+        int size;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter size of array: ");
+        size=sc.nextInt();
+        int[] array;
+        array = new int[size];
+        for (int i=0;i<size;i++) {
+            System.out.println("Enter element "+i);
+            array[i]=sc.nextInt();
+        }
+        System.out.printf("%-20s%s", "Elements in array: ", "");
+        for (int j=0;j<size;j++) {
+            System.out.print(array[j]+"\t");
+        }
+        System.out.println();
+        int min=minValue(array);
+        System.out.println("The smallest of array is "+min);
     }
 }
